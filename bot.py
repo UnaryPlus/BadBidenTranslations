c566465360dbdaac1b495834fb6b7d37239ae3f5
UnaryPlus <83139254+UnaryPlus@users.noreply.github.com>
2021-11-10 15:39:50 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,6 +21,7 @@ class Bot(tweepy.StreamListener):
             # tweepy throws a timeout error every hour or so
             # i'm not sure why, but this keeps the program from halting
             print('streaming ended due to error')
+            time.sleep(120)
             self.start_stream()
 
     def bad_translation(self, text):
